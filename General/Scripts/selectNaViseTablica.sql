87efdd0579109369c8e50d601ab30bb2f8646fab
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-11-28 20:20:14 +0100

    selecetNaviše tablica - sredina + dbeaver

---
+++ General/Scripts/selectNaViseTablica.sql
@@ -0,0 +1,33 @@
+select * from smjer;
+
+select * from grupa where smjer=1;
+
+select a.naziv as smjer, b.naziv as grupa
+from smjer a 
+inner join grupa b 
+on a.sifra=b.smjer
+where a.naziv='PHP programiranje';
+
+
+select a.naziv as smjer, b.naziv as grupa
+from smjer a inner join grupa b 
+on a.sifra=b.smjer;
+
+insert into smjer 
+values (null,'Serviser',11999.99,500,130,false);
+
+select a.naziv as smjer,
+b.naziv as grupa, 
+d.ime, d.prezime
+from smjer a 
+inner join grupa b on a.sifra=b.smjer
+inner join predavac c on b.predavac=c.sifra
+inner join osoba d on c.osoba=d.sifra
+where a.naziv='PHP programiranje';
+
+select b.sifra
+from osoba a inner join predavac b on a.sifra = b.osoba 
+where a.ime = 'Tomislav' and a.prezime = 'Jakopec'; 
+
+update grupa set predavac = 1
+where naziv = 'PP26';

94a9032f3c660ab38ebafb324b1f42d189610939
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-12-05 21:58:34 +0100

    Create kolokvij_vjezba_2.sql

---
+++ Kolokvij_vjezba_2/kolokvij_vjezba_2.sql
@@ -0,0 +1,6 @@
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 <C:\Users\Filip\Documents\Edunova-2022\Kolokvij_vjezba_2\kolokvij_vjezba_2.sql
+
+drop database if exists kolokvij_vjezba_2;
+create database kolokvij_vjezba_2;
+use kolokvij_vjezba_2;
+

a4f36b169c84b2f707e95da1fde213ecd6254b55
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-01-13 11:56:28 +0100

    Update zadatak5.php

--- Edunovapp26/www/ucenjephp.hr/zadatak5.php
+++ Edunovapp26/www/ucenjephp.hr/zadatak5.php
@@ -13,7 +13,7 @@ echo '<hr>';
 
 
 $i = 1; $j = 2; $l = 3;
-$i = ++$i + $j - $l--; // i= j= l=
-$j += ++$l + $i - --$j; // i= j= l=
-$l -= --$j - $l + ++$i; // i= j= l=
-echo $i + $j - $l; // 
+echo $i = ++$i + $j - $l--; // i= j= l=
+echo $j += ++$l + $i - --$j; // i= j= l=
+echo $l -= --$j - $l + ++$i; // i=1 j=4 l=1
+echo $i + $j - $l; // 4

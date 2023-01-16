4e81e50c674df4597216ed66f7215cf62c4806f0
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-01-16 11:11:53 +0100

    Update zadatak5.php

--- Edunovapp26/www/ucenjephp.hr/zadatak5.php
+++ Edunovapp26/www/ucenjephp.hr/zadatak5.php
@@ -12,34 +12,34 @@ echo ++$i + $j--; //8
 echo '<hr>';
 
 $i = 1; $j = 2;
-echo $i = ++$i + $j;   // 
-echo $j = $i - --$j;   //
-echo $i + $j;          // 
+echo $i = ++$i + $j;   // i=4 j=4
+echo $j = $i - --$j;   // i=4 j=3
+echo $i + $j;          // 7
+
+echo '<hr>';
+
+$i = 1; $j = 3;
+echo $i = $i + $j;    // i=4 j=3
+echo $j = $i - --$j;  // i=4 j=2
+echo $i + ++$j;       // 7
 
 echo '<hr>';
 
 $i = 1; $j = 2;
-echo $i = $i + $j;    // 
-echo $j = $i - --$j;  // 
-echo $i + ++$j;       // 
-
-echo '<hr>';
-
-$i = 1; $j = 2;
-echo $i += $i + $j++;    // 
-echo $j -= $i - --$j;    // 
-echo $i++ + ++$j;        // 
+echo $i += $i + $j++;    // i=4 j=2
+echo $j -= $i - --$j;    // i=3 j=0
+echo $i++ + ++$j;        // 5
 
 echo '<hr>';
 
 $i=3; $j=2;
-echo $i += ++$j - $i; //
-echo $j = --$i + $j; //
-echo ++$i + $j--; //
+echo $i = ++$j - $i; // i=0 j=3
+echo $j = --$i + $j; // i=-1 j=2
+echo ++$i + $j; //2
 
 echo '<hr>';
 
-$i = 1; $j = 2;
-echo $i = $i++ + $j;    // 
-echo $j = --$i - --$j;  // 
-echo $i + ++$j;       //
+$i = 2; $j = 2;
+echo $i = $i + $j;    // i=4 j=4
+echo $j = $i - --$j;  // i=4 j=3
+echo $i + ++$j;       //8

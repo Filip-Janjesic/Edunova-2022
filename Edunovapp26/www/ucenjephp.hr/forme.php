b7ccf2876d088c87c97552a878740e4d2047b3b9
Filip Janješić <filip.janjesic@gmail.com>
2023-01-18 21:00:12 +0100

    2. dio

--- Edunovapp26/www/ucenjephp.hr/forme.php
+++ Edunovapp26/www/ucenjephp.hr/forme.php
@@ -1,4 +1,5 @@
 <?php 
+$gradovi=['Valpovo','Osijek','Zagreb','Donji Miholjac'];
 if($_SERVER['REQUEST_METHOD']==='POST'){
   $pb=(int)$_POST['pb'];
   $db=(int)$_POST['db'];
@@ -13,15 +14,47 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
   }else{
     $rez='';
   }
+
+  if(isset($_POST['voce'])){
+    $voce=$_POST['voce'];
+  }else{
+    $voce='';
+  }
+
+  if(isset($_POST['povrce'])){
+    $povrce=$_POST['povrce'];
+    //echo '<pre>';
+    //print_r($povrce);
+    //echo '</pre>';
+  }else{
+    $povrce=[];
+  }
+
+  $grad=$_POST['grad'];
+
+  $opis=$_POST['opis'];
+
+
+  $datum = $_POST['datum'];
+
 }else{
   $pb='';
   $db='';
   $rez='';
+  $voce='';
+  $povrce=[];
+  $grad=$gradovi[0];
+  $opis='';
+  $datum = date('Y-m-d',time());
 }
 //echo '<pre>';
 //print_r($_SERVER);
 //echo '</pre>';
 
+
+//echo '<pre>';
+//print_r($_FILES);
+//echo '</pre>';
 
 ?>
 
@@ -41,7 +74,8 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
         
 
           <form action="<?=$_SERVER['PHP_SELF']?>" 
-          method="post">
+          method="post"
+          enctype="multipart/form-data">
 
           <!-- Foundation RWD style, nije baš po PS -->
           <label> Prvi broj
@@ -52,6 +86,72 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
           <label for="db">Drugi broj</label>
           <input type="text" name="db" id="db" value="<?=$db?>">
           <h1><?=$rez?></h1>
+
+
+          <input type="radio" name="voce" 
+            <?php if($voce==='jabuka'):?>
+              checked="checked"
+            <?php endif;?>
+            id="jabuka" value="jabuka">
+          <label for="jabuka">Jabuka</label>
+          
+          <br>
+
+          <input type="radio" name="voce" 
+          <?php if($voce==='kruska'):?>
+              checked="checked"
+            <?php endif;?>
+            id="kruska" value="kruska">
+          <label for="kruska">Kruška</label>
+
+          <br>
+          <?=$voce?>
+
+          <hr>
+
+          <input type="checkbox" name="povrce[]" 
+          <?php if(in_array('kupus',$povrce)):?>
+              checked="checked"
+            <?php endif;?>
+          id="kupus" value="kupus">
+          <label for="kupus">Kupus</label>
+
+          <input type="checkbox" name="povrce[]" 
+          <?php if(in_array('mrkva',$povrce)):?>
+              checked="checked"
+            <?php endif;?>
+          id="mrkva" value="mrkva">
+          <label for="mrkva">Mrkva</label>
+
+          <hr>
+
+          <label for="grad">Grad</label>
+          <select name="grad" id="grad">
+              <?php foreach($gradovi as $g): ?>
+                <option 
+                <?php if($grad===$g):?>
+              selected="selected"
+            <?php endif;?>
+                value="<?=$g?>"><?=$g?></option>
+                <?php endforeach; ?>
+          </select>
+
+          <label for="opis">Opis</label>
+          <textarea name="opis" id="opis" 
+          cols="30" rows="10"><?=$opis?></textarea>
+
+
+          <label for="slika1">Slika 1</label>
+          <input type="file" name="slika1" id="slika1">
+
+          <br>
+          <label for="slika2">Slika 2</label>
+          <input type="file" name="slika2" id="slika2">
+
+          <label for="datum">Datum</label>
+          <input type="date" name="datum" id="datum"
+          value="<?=$datum?>">
+
           <input class="success button expanded" type="submit" value="Izračunaj">
         
           </form>

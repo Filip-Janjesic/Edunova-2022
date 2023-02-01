a3b7a594950e9dc92c3b6ec2b6a800ef495fbae7
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-01 22:12:10 +0100

    predavanje

--- Edunovapp26/www/ucenjephp.hr/terminal/Pomocno.php
+++ Edunovapp26/www/ucenjephp.hr/terminal/Pomocno.php
@@ -14,13 +14,16 @@ class Pomocno{
         }
     }
 
-    public static function unosTeksta($poruka){
+    public static function unosTeksta($poruka,$vrijednost=''){
         while(true){
             $s=readline($poruka);
             $s=trim($s);
-            if(strlen($s)===0){
+            if(strlen($s)===0 && $vrijednost===''){
                 echo 'Obavezan unos' . PHP_EOL;
                 continue;
+            }
+            if(strlen($s)===0 && $vrijednost!==''){
+                return $vrijednost;
             }
             return $s;
         }

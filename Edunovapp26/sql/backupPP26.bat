a78a9cd9c259cd9f83f05244c3bbce9d732ebb8e
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-15 23:31:18 +0100

    predavanje

--- Edunovapp26/sql/backupPP26.bat
+++ Edunovapp26/sql/backupPP26.bat
@@ -9,9 +9,9 @@ SET dtStamp24=%date:~-5,4%%date:~7,2%%date:~4,2%%time:~0,2%%time:~3,2%%time:~6,2
 
 if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
 
-set naziv=BackupPP27_%dtStamp%.sql
+set naziv=BackupPP26_%dtStamp%.sql
 
-c:\xampp\mysql\bin\mysqldump -uroot edunovaPP27 > e:\%naziv%
+c:\xampp\mysql\bin\mysqldump -uroot edunovapp26 > e:\%naziv%
 
 
 ECHO "GOTOV"

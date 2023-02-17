ef4918702726aed9e156b74e519826140815ff53
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-17 21:45:40 +0100

    Update index.php

--- Edunovapp26/autorizacija/index.php
+++ Edunovapp26/autorizacija/index.php
@@ -3,26 +3,6 @@ $email = isset($_GET['email']) ? $_GET['email'] : (isset($_COOKIE['email']) ? $_
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Javno</title>
-</head>
-<body>
-    Javni dio
-    <form action="autorizacija.php" method="post">
-        <input type="text" name="email"
-        value="<?=$email?>"
-        placeholder="email" id="">
-        <input type="password" name="lozinka" id="">
-        <input type="submit" value="Autoriziraj se">
-    </form>
-</body>
-</html>
-
-<!DOCTYPE html>
 <html>
   <head>
       <meta charset="UTF-8">

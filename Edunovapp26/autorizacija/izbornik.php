f3dcb83636ee1008a2ebd72fe338b2b8d55fd32e
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-18 19:56:13 +0100

    Create izbornik.php

---
+++ Edunovapp26/autorizacija/izbornik.php
@@ -0,0 +1,22 @@
+<div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle></button>
+  <div class="title-bar-title">Menu</div>
+</div>
+
+<div class="top-bar" id="example-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li>
+        <a href="index.php">Polaznik 40</a>
+      </li>
+        <a href="#0">Zadaci</a>
+        <ul class="submenu menu vertical" data-submenu>
+          <li><a target="_blank" href="zadatak3.php">Zadatak 3</a></li>
+          <li><a target="_blank" href="ciklicnamatrica.hr/index.php">Ciklična matrica</a></li>
+          <li><a target="_blank" href="ljubavnikalkulator.hr/index.php">Ljubavni kalkulator</a></li>
+          <li><a target="_blank" href="zadatak6.php">Ispiši zbroj svih primljenih brojeva</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>

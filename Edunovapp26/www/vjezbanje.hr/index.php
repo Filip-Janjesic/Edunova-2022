2a2eb83d963ad28ec7caf6150693cad1d8ba8865
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-02-21 23:20:28 +0100

    vjezbanje

---
+++ Edunovapp26/www/vjezbanje.hr/index.php
@@ -0,0 +1,32 @@
+<?php
+
+define('BP',__DIR__ . DIRECTORY_SEPARATOR);
+define('BP_APP', BP . 'app' . DIRECTORY_SEPARATOR);
+
+$zaAutoload=[
+    BP_APP . 'controller',
+    BP_APP . 'core',
+    BP_APP . 'model'
+];
+
+$putanje = implode(PATH_SEPARATOR,$zaAutoload);
+
+set_include_path($putanje);
+
+spl_autoload_register(function($klasa){
+
+    $putanje = explode(PATH_SEPARATOR,get_include_path());
+    foreach($putanje as $putanja){
+        //echo $putanja . '<br>';
+        $datoteka = $putanja . DIRECTORY_SEPARATOR .
+                        $klasa . '.php';
+        //echo $datoteka, '<br>';
+        if(file_exists($datoteka)){
+            require_once $datoteka;
+            break;
+        }
+    }
+});
+
+App::start();
+
